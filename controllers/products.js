dcc98343c4b12266e033a116b23b47b444830352
unknown <hannanmalik1999@gmail.com>
2023-01-19 14:30:04 +0100

    Product Schema and basic controller setup

---
+++ controllers/products.js
@@ -0,0 +1,23 @@
+const Product = require("../models/product")
+
+const getAllProductsStatic = async (req,res) => {
+    const products = await Product.find({})
+    res.status(200).json({products})
+}
+
+const getAllProducts = async (req,res) => {
+    const {featured} = req.query
+    const queryObject = {}
+    if(featured){
+        queryObject.featured = featured === "true"? true: false
+    }
+    const products = await Product.find(queryObject)
+    res.status(200).json({products, nbHits: products.length})
+}
+
+
+
+module.exports = {
+    getAllProductsStatic,
+    getAllProducts
+}
